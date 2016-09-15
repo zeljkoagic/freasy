c3e555d5d5f7f0e3f32b25c4a225286ccf395196
Zeljko Agic <zeljko.agic@gmail.com>
2016-09-15 16:37:10 +0200

    x

--- src/merge_tags.py
+++ src/merge_tags.py
@@ -4,13 +4,11 @@ gold = open(sys.argv[1]).readlines()
 system = open(sys.argv[2]).readlines()
 
 for g, s in zip(gold, system):
-    g = g.strip()
-    s = s.strip()
-    if g and s and len(g.split()) > 1:
-        gl = g.split()
-        sl = s.split()
-
-	print("%s\t%s\t_\t_\t%s\t_\t_\t_\t%s\t_\t%s\t_\t_" % (gl[0], gl[1], sl[5], gl[8], gl[10]))  # conll 2009 lex
-	# print("%s\t_\t_\t_\t%s\t_\t_\t_\t%s\t_\t%s\t_\t_" % (gl[0], sl[5], gl[8], gl[10]))  # conll 2009 delex
-    else:
-        print
+	g = g.strip()
+	s = s.strip()
+	if g and s and len(g.split()) > 1:
+		gl = g.split()
+		sl = s.split()
+		print("%s\t%s\t_\t_\t%s\t_\t_\t_\t%s\t_\t%s\t_\t_" % (gl[0], gl[1], sl[5], gl[8], gl[10]))  # conll 2009 lex
+	else:
+		print()
