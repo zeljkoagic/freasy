ff34d918301e6e84a8e25bf3621ed730163077d8
Zeljko Agic <zeljko.agic@gmail.com>
2016-09-17 12:38:25 +0200

    x

--- src/freasy/vote.py
+++ src/freasy/vote.py
@@ -53,7 +53,7 @@ for sentence in target_sentences:
 
         # FIXME This will not work, we need to index by source!!!
         if source != "ALL":
-            tensor[:, :, idx] *= math.pow(1 / source_weights["wals"][10][sentence.idx][1][source], 2)
+            tensor[:, :, idx] *= math.pow(1 / source_weights["wals"][10][sentence.idx][1][source], 3)
 
         heads, _ = chu_liu_edmonds(tensor[:, :, idx])
         heads = heads[1:]
