bb7030e71b97d6d0a2be4310ee25b185f30d38c4
Zeljko Agic <zeljko.agic@gmail.com>
2016-09-19 12:02:45 +0200

    x

--- src/freasy/softmax.py
+++ src/freasy/softmax.py
@@ -1,17 +1,20 @@
 import numpy as np
 
 
-def softmax(sources_distribution, temperature=1.0, power=1.0):
-    sources = [s for s, _ in sources_distribution]
-    weights = np.array([w for _, w in sources_distribution])
+#def softmax(sources_distribution, temperature=1.0):
+#    sources = [s for s, _ in sources_distribution]
+#    weights = np.array([w for _, w in sources_distribution])
+#
+#    e = np.exp(weights / temperature)
+#    softmaxed = e / np.sum(e)
+#
+#    sources_distribution_softmaxed = list(zip(sources, softmaxed))
+#
+#    return sources_distribution_softmaxed
 
-    e = np.exp(weights / temperature)
-    softmaxed = e / np.sum(e)
-    # powered = np.power(softmaxed, [power])
-
-    sources_distribution_softmaxed = list(zip(sources, softmaxed))
-
-    return sources_distribution_softmaxed
+def softmax(sentence_matrix, temperature=1.0):
+    m_exp = np.exp(sentence_matrix/temperature)
+    return (m_exp.T / np.sum(m_exp, axis=1)).T
 
 
 def invert(sources_distribution):
--- src/freasy/vote.py
+++ src/freasy/vote.py
@@ -13,12 +13,13 @@ from dependency_decoding import chu_liu_edmonds
 import dill
 from collections import defaultdict
 import math
+import argparse
 
 # FIXME Maybe better to softmax entire matrix? Softmax makes sense after the language weights are applied.
 from softmax import softmax
 
 
-def load_tensor(n, arcs):
+def load_tensor(n, arcs, pos_source):
     """
     Creates a tensor from a list of dependency arcs.
     """
@@ -31,37 +32,70 @@ def load_tensor(n, arcs):
     for lang_index, (lang, lang_arcs) in enumerate(arcs.items()):
         sources.append(lang)  # store the languages in a particular order
 
-        for arc in lang_arcs["proj"]:
+        for arc in lang_arcs[pos_source]:
             tensor[arc.dependent, arc.head, lang_index] = arc.weight  # fill the tensor with weights
 
     return tensor, sources
 
-# load the target sentence pickle
-target_sentences = dill.load(open(sys.argv[1], "rb"))
+# argparse stuff
+parser = argparse.ArgumentParser(description="Performs language weighting experiments.")
+parser.add_argument("--target_name", required=True, help="target language name")
+parser.add_argument("--data_root", required=True, help="root for data files")
+parser.add_argument("--pos_source", required=True, choices=["gold", "pred", "proj"], help="POS source")
+parser.add_argument("--weighting_method", required=True, choices=["klcpos3", "wals", "langid"],
+                    help="source weighting method")
+parser.add_argument("--granularity", required=True, help="target language estimation granularity", type=int)
+parser.add_argument('--use_softmax', required=True, choices=[0, 1],
+                    help="use softmax to smooth source contributions?", type=int)
+parser.add_argument("--temperature", required=False, help="softmax temperature", type=float)
+args = parser.parse_args()
 
-# FIXME load the source weights
-source_weights = dill.load(open(sys.argv[2], "rb"))
+# load the target sentence pickle
+target_sentences = dill.load(open("{}/pickles/{}.as_target_language.all_parses.pickle"
+                                  .format(args.data_root, args.target_name), "rb"))
+
+# load the source weights
+source_weights = dill.load(open("{}/pickles/{}.source_language_mappings.with_{}_pos.pickle"
+                                .format(args.data_root, args.target_name, args.pos_source), "rb"))
+
+# TODO Isn't this now redundant?
+source_weights_for_method_and_granularity = source_weights[args.weighting_method][args.granularity]
+
+assert args.granularity in source_weights[args.weighting_method], \
+    "You must choose one of these as granularity: %s" % source_weights[args.weighting_method].keys()
+
+assert bool(args.use_softmax) == bool(args.temperature), "If args.softmax, then args.temperature as well!"
 
 correct = defaultdict(int)
 total = defaultdict(int)
 
 # process each sentence
 for sentence in target_sentences:
-    tensor, sources = load_tensor(len(sentence.tokens), sentence.arcs_from_sources)
+
+    # create tensor from arcs
+    tensor, sources = load_tensor(len(sentence.tokens), sentence.arcs_from_sources, args.pos_source)
+
+    # extract the weights
+    best_source_for_sentence, source_weights_for_sentence = source_weights_for_method_and_granularity[sentence.idx]
+
+    # apply softmax
+    if args.softmax:
+        source_weights_for_sentence = softmax(source_weights_for_sentence, temperature=args.temperature)
 
     # here we decode for the individual sources
-    # TODO This decoding is trivial because individual slices are already trees! Makes sense only to decode for voted.
     # source order is important because the tensor is not explicitly indexed by source names
     for idx, source in enumerate(sources):
 
-        # FIXME This will not work, we need to index by source!!!
-        #if source != "ALL":
-        #    tensor[:, :, idx] *= math.pow(1 / source_weights["wals"][100][sentence.idx][1][source], 2)
+        # apply weights
+        if source != "ALL" and args.softmax:
+            tensor[:, :, idx] *= source_weights_for_sentence[source]
 
+        # TODO Individual slices are already trees! Makes sense only to decode for voted.
         heads, _ = chu_liu_edmonds(tensor[:, :, idx])
         heads = heads[1:]
 
-        correct[source] += sum([predicted == gold for predicted, gold in zip(heads, [arc.head for arc in sentence.gold_arcs])])
+        correct[source] += sum([predicted == gold for predicted, gold
+                                in zip(heads, [arc.head for arc in sentence.gold_arcs])])
         total[source] += len(sentence.tokens)
 
     # this is where voting happens, currently all weights are 1.0
@@ -70,7 +104,8 @@ for sentence in target_sentences:
     heads, _ = chu_liu_edmonds(voted)
     heads = heads[1:]
 
-    correct["voted"] += sum([predicted == gold for predicted, gold in zip(heads, [arc.head for arc in sentence.gold_arcs])])
+    correct["voted"] += sum([predicted == gold for predicted, gold
+                             in zip(heads, [arc.head for arc in sentence.gold_arcs])])
     total["voted"] += len(sentence.tokens)
 
 
