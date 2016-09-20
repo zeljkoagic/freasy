8e177a393d4e1c63a47f0821c534ddcfdd88d696
Zeljko Agic <zeljko.agic@gmail.com>
2016-09-20 18:07:37 +0200

    x

--- src/freasy/vote.py
+++ src/freasy/vote.py
@@ -33,7 +33,7 @@ def load_tensor(n, arcs, pos_source):
             # multisource gets special treatment
             multi_source_matrix[arc.dependent, arc.head] = arc.weight
 
-    return single_source_tensor, multi_source_matrix, sources
+    return single_source_tensor, single_source_tensor, multi_source_matrix, sources
 
 
 def get_heads(matrix):
@@ -110,10 +110,10 @@ for sentence in target_sentences:
                     # TODO Find *true* best single source---WHICH GRANULARITY?
 
                     # create tensor from arcs
-                    ss_tensor, ms_matrix, sources = load_tensor(len(sentence.tokens), sentence.arcs_from_sources,
-                                                                pos_source)
+                    ss_tensor, ss_tensor_weighted, ms_matrix, sources = \
+                        load_tensor(len(sentence.tokens), sentence.arcs_from_sources, pos_source)
 
-                    ss_tensor_weighted = ss_tensor  # for weighting
+
                     heads_ms = get_heads(ms_matrix)
                     correct_ms += count_correct(heads_ms, heads_gold)
 
