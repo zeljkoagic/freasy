96c11538b9524f6397c397e85df4028e98f6ce55
Zeljko Agic <zeljko.agic@gmail.com>
2016-09-20 18:40:22 +0200

    x

--- src/freasy/vote.py
+++ src/freasy/vote.py
@@ -110,9 +110,6 @@ total = 0
 for sentence in target_sentences:
 
     heads_gold = get_heads(get_matrix(len(sentence.tokens), sentence.gold_arcs))
-    print(get_matrix(len(sentence.tokens), sentence.gold_arcs))
-    print(sentence.tokens)
-    print(heads_gold)
 
     for pos_source in pos_sources:
         for weighting_method in weighting_methods:
