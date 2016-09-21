ab2344bc93b392664c176340a0c72727dc284da4
Zeljko Agic <zeljko.agic@gmail.com>
2016-09-21 21:46:57 +0200

    x

--- src/freasy/vote.py
+++ src/freasy/vote.py
@@ -119,8 +119,8 @@ for source_language, correct_heads in ss_correct.items():
 
 print(true_best_single_source, "{0:.2f}".format((ss_correct[true_best_single_source]/total)*100))
 
-print("ss oracle: {0:.2f}".format((ss_oracle_correct/total)*100), sorted(ss_oracle_sources_counter.items(), key=operator.itemgetter(1), reversed=True))
-print("ss predicted: {0:.2f}".format((ss_predicted_correct/total)*100), sorted(ss_predicted_sources_counter.items(), key=operator.itemgetter(1), reversed=True))
+print("ss oracle: {0:.2f}".format((ss_oracle_correct/total)*100), sorted(ss_oracle_sources_counter.items(), key=operator.itemgetter(1), reverse=True))
+print("ss predicted: {0:.2f}".format((ss_predicted_correct/total)*100), sorted(ss_predicted_sources_counter.items(), key=operator.itemgetter(1), reverse=True))
 
 print("ms: {0:.2f}".format((ms_correct/total)*100))
 print("vote w=1: {0:.2f}".format((ss_voted_unweighted_correct/total)*100))
