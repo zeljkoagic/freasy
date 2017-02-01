247137164cd59f7279bfa40d5654d80ca133aadf
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-01 15:04:30 +0100

    x

--- src/freasy/vote.py
+++ src/freasy/vote.py
@@ -154,7 +154,7 @@ for sentence in target_sentences:
 
     #for l, v in ss_correct_for_this_sentence.items():
     #    ss_correct_for_this_sentence[l] = v / len(sentence.tokens)
-    print(sentence.predicted_pos, sorted(ss_correct_for_this_sentence.items()))
+    print(sentence.predicted_pos, ss_correct_for_this_sentence)
 
     # collect where heads come from
     for tokenid, chosen_head in enumerate(ss_voted_weighted_heads[1:]):
