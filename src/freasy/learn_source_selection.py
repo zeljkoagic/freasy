bb884bd0701def094da14efdde0eb9921afe9fe7
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-03 13:34:34 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -79,7 +79,7 @@ model.add(Bidirectional(LSTM(64)))
 model.add(Dropout(0.5))
 model.add(Dense(26, activation='sigmoid'))
 
-model.compile('adam', 'kullback_leibler_divergence', metrics=['kullback_leibler_divergence'])
+model.compile('adam', 'kullback_leibler_divergence')
 
 print('Train...')
 model.fit(X_train, Y_train,
