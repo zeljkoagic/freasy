77b22a09b799392e9dfac7dbbad0af106a80c97a
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-03 12:30:45 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -1,1 +1,20 @@
 # here we use a recurrent neural network to learn the target->source mapping
+
+import argparse
+import dill
+
+parser = argparse.ArgumentParser(description="TODO")
+parser.add_argument("--data_root", required=True, help="root for data files")
+parser.add_argument("--pos_source", required=True, choices=["gold", "pred", "proj"], help="POS source")
+args = parser.parse_args()
+
+dev_langs = ["en", "de", "es"]
+
+training_data = []
+
+for lang in dev_langs:
+    target_sentences = dill.load(open("{}/pickles/target_lang_{}.pos_source_{}.nn_training_data"
+                                      .format(args.data_root, lang, args.pos_source), "rb"))
+    training_data.append(target_sentences)
+
+print(len(training_data))
