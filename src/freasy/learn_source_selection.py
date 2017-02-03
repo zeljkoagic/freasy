a8afee34f048745ea03b2d06c1946869950ea031
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-03 13:29:30 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -67,3 +67,20 @@ Y_train = np.array(Y_train)
 print(X_train.shape, Y_train.shape)
 
 print(X_train[0], Y_train[0])
+
+print("Pad sequences (samples x time)")
+X_train = sequence.pad_sequences(X_train, maxlen=100)
+print('X_train shape:', X_train.shape)
+
+model = Sequential()
+model.add(Embedding(20000, 128, input_length=100))
+model.add(Bidirectional(LSTM(64)))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', 'kullback_leibler_divergence', metrics=['accuracy'])
+
+print('Train...')
+model.fit(X_train, Y_train,
+          batch_size=32,
+          nb_epoch=4)
