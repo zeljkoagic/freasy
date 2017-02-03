cb600dafa845e42b495c985048dfe8e1d36d7d55
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-03 13:31:56 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -74,10 +74,10 @@ X_train = sequence.pad_sequences(X_train, maxlen=100)
 print('X_train shape:', X_train.shape)
 
 model = Sequential()
-model.add(Embedding(20000, 128, input_length=100))
+#model.add(Embedding(20000, 128, input_length=100))
 model.add(Bidirectional(LSTM(64)))
 model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(26, activation='sigmoid'))
 
 model.compile('adam', 'kullback_leibler_divergence', metrics=['accuracy'])
 
