d8531e40d938a51585f30a92c3de320e0e04ea4c
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-03 18:34:33 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -102,7 +102,7 @@ model = Sequential()
 model.add(Embedding(40000, 512, input_length=100))
 model.add(Bidirectional(LSTM(128, activation="sigmoid")))
 model.add(Dropout(0.5))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(26, activation='softmax'))
 
 model.compile('adam', 'kullback_leibler_divergence', metrics=['accuracy'])
 
