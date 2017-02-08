05221473366952cc6f79565b41372b83842d18f8
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-08 14:24:17 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -103,7 +103,7 @@ print(X_train_reshaped)
 
 model = Sequential()
 
-model.add(Embedding(40000, 512))
+model.add(Embedding(17, 512))
 
 model.add(LSTM(128, activation="sigmoid", input_shape=(3228, 100, 10), return_sequences=True))
 model.add(Dropout(0.2))
