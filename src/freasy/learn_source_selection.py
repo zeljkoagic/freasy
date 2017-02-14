f7b5f8d21ad07fec5ea22b4af0ac244c639a620a
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-14 16:45:53 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -146,5 +146,4 @@ model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 print('Train...')
 model.fit(X_train_reshaped, Y_train,
           batch_size=32,
-          nb_epoch=10,
-          validation_data=[X_test, Y_test])
+          nb_epoch=10)
