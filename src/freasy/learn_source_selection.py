253026b17a3223aa604968c9a6d73ff04454a9f5
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-16 12:30:01 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -25,30 +25,6 @@ for lang in dev_langs:
                                       .format(args.data_root, lang, args.pos_source), "rb"))
     training_data += target_sentences
 
-# 1. map data to one-hot
-# 2. create arch
-# 3. train
-
-one_hot = {
-    "ADJ":   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "ADP":   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "ADV":   [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "AUX":   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "CONJ":  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "DET":   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "INTJ":  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "NOUN":  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "NUM":   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    "PART":  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    "PRON":  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    "PROPN": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    "PUNCT": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    "SCONJ": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    "SYM":   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    "VERB":  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    "X":     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-}
-
 one_hot = {
     "ADJ":   1,
     "ADP":   2,
@@ -123,25 +99,25 @@ model = Sequential()
 #model.add(Dense(output_dim=64,
 #                input_dim=128, activation="relu"))
 
-model.add(GRU(output_dim=64,
-               input_dim=1,
-               input_length=10,
-               activation="relu",
-               return_sequences=True))
+model.add(Bidirectional(LSTM(output_dim=64,
+                             input_dim=1,
+                             input_length=10,
+                             activation="relu",
+                             return_sequences=True)))
 
 Dropout(0.5)
 
-model.add(GRU(output_dim=31,
-               input_dim=64,
-               input_length=10,
-               activation="relu",
-               return_sequences=False))
+model.add(Bidirectional(LSTM(output_dim=32,
+                             input_dim=64,
+                             input_length=10,
+                             activation="relu",
+                             return_sequences=False)))
 
 Dropout(0.5)
 
 model.add(Dense(10, activation='softmax'))
 
-model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
+model.compile('rmsprop', 'categorical_crossentropy', metrics=['accuracy'])
 
 
 #output_array = model.predict(X_train)
