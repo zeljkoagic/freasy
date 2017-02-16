62721bc6a9bbb58a4f97d73d801fbdfcc55558c4
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-16 15:11:35 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -16,8 +16,11 @@ parser.add_argument("--data_root", required=True, help="root for data files")
 parser.add_argument("--pos_source", required=True, choices=["gold", "pred", "proj"], help="POS source")
 args = parser.parse_args()
 
+# all languages have this data
+# we select only the dev set for training
 dev_langs = ["en", "es", "de", "fr", "it", "hi", "hr", "cs", "he", "id"]
 
+# TODO split train and dev into two datasets, one is for training, other for validation
 training_data = []
 
 for lang in dev_langs:
@@ -25,25 +28,8 @@ for lang in dev_langs:
                                       .format(args.data_root, lang, args.pos_source), "rb"))
     training_data += target_sentences
 
-one_hot = {
-    "ADJ":   1,
-    "ADP":   2,
-    "ADV":   3,
-    "AUX":   4,
-    "CONJ":  5,
-    "DET":   6,
-    "INTJ":  7,
-    "NOUN":  8,
-    "NUM":   9,
-    "PART":  10,
-    "PRON":  11,
-    "PROPN": 12,
-    "PUNCT": 13,
-    "SCONJ": 14,
-    "SYM":   15,
-    "VERB":  16,
-    "X":     17
-}
+one_hot = {"ADJ": 1, "ADP": 2, "ADV": 3, "AUX": 4, "CONJ": 5, "DET": 6, "INTJ": 7, "NOUN": 8, "NUM": 9,
+           "PART": 10, "PRON": 11, "PROPN": 12, "PUNCT": 13, "SCONJ": 14, "SYM": 15, "VERB": 16, "X": 17}
 
 X_train = []
 Y_train = []
@@ -55,23 +41,21 @@ for item in training_data:
         all.append(one_hot[pos])
 
     all = np.array(all, dtype=float)
-   #all /= 17.0
 
     X_train.append(all)
     ranks[lang] = 0
     ranks = softmax(ranks)
-    yval = np.array([x for y, x in sorted(ranks.items(), key=operator.itemgetter(0), reverse=False) if y in dev_langs], dtype=float)
+    yval = np.array([x for y, x in sorted(ranks.items(), key=operator.itemgetter(0), reverse=False) if y in dev_langs],
+                    dtype=float)
     am = np.argmax(yval)
     yval2 = [float(i == am) for i, _ in enumerate(yval)]
     Y_train.append(yval2)
-    #print(yval2)
 
 X_train = np.array(X_train[:-100])
 Y_train = np.array(Y_train[:-100])
 
 X_test = np.array(X_train[-100:])
 Y_test = np.array(Y_train[-100:])
-
 
 print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
 print("Pad sequences (samples x time)")
@@ -84,26 +68,28 @@ X_test_reshaped = np.reshape(X_test, (100, 10, 1))
 
 model = Sequential()
 
-#model.add(Embedding(input_dim=18,
+# model.add(Embedding(input_dim=18,
 #                    output_dim=128,
 #                    input_length=10))
 
 
 model.add(LSTM(output_dim=64,
-                             input_dim=1,
-                             input_length=10,
-                             activation="relu",
-                             return_sequences=True,
-                             dropout_U=0.2,
-                             dropout_W=0.2))
+               input_dim=1,
+               input_length=10,
+               activation="relu",
+               return_sequences=True#,
+               #dropout_U=0.2,
+               #dropout_W=0.2
+        ))
 
 model.add(LSTM(output_dim=32,
-                             # input_dim=64,
-                             # input_length=10,
-                             activation="relu",
-                             return_sequences=False,
-                             dropout_U=0.2,
-                             dropout_W=0.2))
+               # input_dim=64,
+               # input_length=10,
+               activation="relu",
+               return_sequences=False#,
+               #dropout_U=0.2,
+               #dropout_W=0.2
+        ))
 
 model.add(Dense(10, activation='softmax'))
 
