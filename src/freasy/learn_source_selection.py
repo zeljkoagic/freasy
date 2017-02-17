ac3c72a12ec998ecdf02d7fe976302504b9300fc
Zeljko Agic <zeljko.agic@gmail.com>
2017-02-18 00:29:55 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -132,7 +132,7 @@ X_test = sequence.pad_sequences(X_test, maxlen=10)
 #print('X_train shape:', X_train.shape)
 
 X_train_reshaped = np.reshape(X_train, (X_train.shape[0], 10, 1))
-X_test_reshaped = np.reshape(X_test, (100, 10, 1))
+X_test_reshaped = np.reshape(X_test.shape[0], (100, 10, 1))
 #print('X_train reshaped:', X_train_reshaped.shape)
 #print(X_train_reshaped)
 
