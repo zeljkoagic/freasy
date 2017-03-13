bbd2ebf1963fdb597527720bfd215f897bf21e12
Zeljko Agic <zeljko.agic@gmail.com>
2017-03-13 14:16:19 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -153,7 +153,7 @@ model.add(Dense(10, activation='softmax'))
 model.compile('adam', 'mse', metrics=['accuracy'])
 
 print('Train...')
-model.fit(X_train, Y_train,
+model.fit(X_train_reshaped, Y_train,
           batch_size=32,
           nb_epoch=100,
-          validation_data=[X_test, Y_test])
+          validation_data=[X_test_reshaped, Y_test])
