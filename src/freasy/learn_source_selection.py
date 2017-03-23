21c41e8e850681908bb6b00b81c434f22c275df6
Zeljko Agic <zeljko.agic@gmail.com>
2017-03-23 13:39:10 +0100

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -141,8 +141,8 @@ model.add(Dense(10, activation='softmax'))
 model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 
 print('Train...')
-model.fit(X_train, Y_train,
+model.fit(X_train_reshaped, Y_train,
           batch_size=16,
           nb_epoch=10,
-          validation_data=[X_test, Y_test])
+          validation_data=[X_test_reshaped, Y_test])
 
