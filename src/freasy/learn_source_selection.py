2b5f504986dbee2135504686d7b1bd841532df52
Zeljko Agic <zeljko.agic@gmail.com>
2018-04-23 10:31:44 +0200

    x

--- src/freasy/learn_source_selection.py
+++ src/freasy/learn_source_selection.py
@@ -82,19 +82,19 @@ for data in [(test_data, X_test, Y_test), (training_data, X_train, Y_train)]:
         # add to training data
         Y.append(y_val.tolist())
 
-X_train = sequence.pad_sequences(X_train, maxlen=64, dtype=float)
-X_test = sequence.pad_sequences(X_test, maxlen=64, dtype=float)
+X_train = sequence.pad_sequences(X_train, maxlen=32, dtype=float)
+X_test = sequence.pad_sequences(X_test, maxlen=32, dtype=float)
 
 model = Sequential()
 
 model.add(Embedding(len(tag_ids)+1, 12))
 
 model.add(Bidirectional(LSTM(units=128,
-                             input_shape=(64, 12),
+                             input_shape=(32, 12),
                              activation="relu",
                              return_sequences=False,
-                             dropout=0.2,
-                             recurrent_dropout=0.2)))
+                             dropout=0.5,
+                             recurrent_dropout=0.5)))
 
 #model.add(Bidirectional(LSTM(units=64,
 #                             input_shape=(128, 64, 12),
@@ -110,6 +110,6 @@ model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 print('Train...')
 model.fit(X_train, Y_train,
           batch_size=32,
-          epochs=100,
+          epochs=5,
           validation_data=[X_test, Y_test])
 
